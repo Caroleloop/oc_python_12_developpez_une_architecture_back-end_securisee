7436280553f2aacaed8b6411d1cbc352eeb510c9
Carole Roch <1rochcarole@gmail.com>
2025-10-14 14:44:03 +0200

    fonction qui permet de se déconnecter

--- app/cli/auth_cli.py
+++ app/cli/auth_cli.py
@@ -1,7 +1,10 @@
 import typer
 from app.auth.core import login
+from pathlib import Path
 
 app = typer.Typer(help="Commandes pour l'authentification")
+
+TOKEN_FILE = Path(".token")
 
 
 @app.command("login")
@@ -14,6 +17,17 @@ def login_user(
     """Se connecter et obtenir un token JWT."""
     try:
         token = login(email, mot_de_passe)
+        TOKEN_FILE.write_text(token)
         typer.echo(f"Token JWT : {token}")
     except ValueError as e:
         typer.echo(f"Erreur : {e}")
+
+
+@app.command("logout")
+def logout_user():
+    """Déconnecte l'utilisateur en supprimant le token local."""
+    if TOKEN_FILE.exists():
+        TOKEN_FILE.unlink()
+        typer.echo("Déconnexion réussie ! Le token a été supprimé.")
+    else:
+        typer.echo("Aucun token trouvé, vous êtes déjà déconnecté.")
